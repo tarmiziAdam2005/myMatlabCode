f1709d30f8feb9d5b7752e48b6f73cab48681cb4
Tarmizi Adam <tarmizi_adam2005@yahoo.com>
2016-12-12 18:15:16 +0800

    Update HOTV_demo.m

--- Image Processing/Second Order TV/HOTV_demo.m
+++ Image Processing/Second Order TV/HOTV_demo.m
@@ -2,18 +2,18 @@ clc
 clear all;
 close all;
 
-Img = double(imread('Lighthouse256.bmp')); %Your Image goes here
+Img = double(imread('boat.bmp')); %Your Image goes here
 
 N = numel(Img);
 %H = ones(9,9)/81;
-K = fspecial('gaussian', [9 9], 5); % Deblurring. Try other kernels as
+%K = fspecial('gaussian', [9 9], 5); % Deblurring. Try other kernels as
                                      % well
-%K     =   fspecial('average',1); % For denoising
+K     =   fspecial('average',1); % For denoising
 f = imfilter(Img,K,'circular');
 
-BSNR =30;
-sigma = BSNR2WGNsigma(f, BSNR);
-
+BSNR =15;
+%sigma = BSNR2WGNsigma(f, BSNR);
+sigma = BSNR;
 % If you dont have the BSNR2WGNsigma() function, use the below instead.
 
 %sigma = sqrt(5);
@@ -23,11 +23,11 @@ f = f +  sigma * randn(size(Img)); %Add a little noise
 
 
 
-opts.lam       = 3500; % for deblurring, try large values.
-opts.omega     = 50;  % reg param for 2nd order TV
+%opts.lam       = 3500; % for deblurring, try large values.
+%opts.omega     = 50;  % reg param for 2nd order TV
 
-%opts.lam       = 1.65; % for denoising try starting with these 
-%opts.omega     = 12;  
+opts.lam       = 0.1; % for denoising try starting with these 
+opts.omega     = 1;  
 
 opts.Nit       = 400;
 opts.tol       = 1.0e-5;
@@ -40,11 +40,11 @@ out = HOTV(f,Img,K, opts);
 figure;
 imshow(uint8(out.sol));
 title(sprintf('2nd Order TV Deblurred (PSNR = %3.3f dB, cputime %.3f s) ',...
-                       psnr_fun(uint8(out.sol),uint8(Img)), out.cpuTime));
+                       psnr_fun(out.sol,Img), out.cpuTime));
 
 figure;
 imshow(uint8(f));
-title(sprintf('Blurred + Noisy (PSNR = %3.3f dB)',psnr_fun(uint8(f),uint8(Img))));
+title(sprintf('Blurred + Noisy (PSNR = %3.3f dB)',psnr_fun(f,Img)));
 
 figure;
 imshow(uint8(Img));
